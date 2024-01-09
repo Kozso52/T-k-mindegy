f8f246b0d442ee594e818799c193de7f20d67e69
Kozso52 <151125918+Kozso52@users.noreply.github.com>
2024-01-09 17:18:49 +0100

    Ez a véletlen függvény

---
+++ véletlenfüggvény.py
@@ -0,0 +1,12 @@
+import sqlite3
+import random
+
+def vél():
+    vél=random.Random()
+    return vél,randrange(2)
+
+def adat(lista,a):
+    return lista[a]
+
+def adat2(lista,a,b):
+    return lista(a)[b]
