5085467e3aedecdc9b29509464a6d3656abc5078
maschekmark17 <151126055+maschekmark17@users.noreply.github.com>
2024-01-10 13:56:41 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -16,6 +16,12 @@ base=sqlite3.connect("szavak.db")
 t=base.cursor()
 adatok=t.execute("Select szó,mondat1,mondat2,mondat3,mondat4 from szavmon")
 datas=[list(adat) for adat in adatok]
-
-print(adat(datas,véletlen(),0))
+szám=[]
+szám.append(véletlen())
+print(szám)
+print(adat(datas,szám[0],0))#szó#
+print(adat(datas,szám[0],1))#mondat1#
+print(adat(datas,szám[0],2))#mondat2#
+print(adat(datas,szám[0],3))#mondat3#
+print(adat(datas,szám[0],4))#mondat4#
 
