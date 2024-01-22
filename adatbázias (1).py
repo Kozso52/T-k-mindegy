c06b5bff75dec50793a6f01c577041ae55cc76af
SosAlex12 <151126117+SosAlex12@users.noreply.github.com>
2024-01-22 13:48:05 +0100

    ADATBÁZIS SÓS

---
+++ adatbázias (1).py
@@ -0,0 +1,33 @@
+import sqlite3
+def adatokbe(szó,mondat1,mondat2,mondat3,mondat4):
+    adatbázis=sqlite3.connect("szavak.db")
+    t=adatbázis.cursor()
+    t.execute("CREATE TABLE IF NOT EXISTS szavmon(szó,mondat1,mondat2,mondat3,mondat4)")
+    para= """ INSERT INTO szavmon (szó,mondat1,mondat2,mondat3,mondat4)
+    VALUES(?,?,?,?,?);"""
+    data= (szó,mondat1,mondat2,mondat3,mondat4)
+    t.execute(para,data )
+    adatbázis.commit()
+    adat=t.execute("Select szó,mondat1 from szavmon")
+    print(adat.fetchall())
+    for adatok in t.execute("Select szó,mondat1,mondat2,mondat3,mondat4 from szavmon"):
+        print(adatok)
+    adatbázis.close()
+    
+def adatka():
+    global szó
+    global mondat1
+    global mondat2
+    global mondat3
+    global mondat4
+    szó=input("szó: ")
+    while szó!="":
+        mondat1=input("Mondat:")
+        mondat2=input("Mondat:")
+        mondat3=input("Mondat:")
+        mondat4=input("Mondat:")
+        adatokbe(szó,mondat1,mondat2,mondat3,mondat4)
+        if szó!="":
+            return adatka()
+adatka()
+
