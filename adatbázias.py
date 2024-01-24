b5eac5104e232e545fa75a49c7a086c6ff1d95d5
maschekmark17 <151126055+maschekmark17@users.noreply.github.com>
2024-01-24 13:18:48 +0100

    Add files via upload

--- adatbázias.py
+++ adatbázias.py
@@ -1,4 +1,6 @@
 import sqlite3
+import hashlib
+
 def adatokbe(szó,mondat1,mondat2,mondat3,mondat4):
     adatbázis=sqlite3.connect("szavak.db")
     t=adatbázis.cursor()
@@ -15,7 +17,8 @@ def adatokbe(szó,mondat1,mondat2,mondat3,mondat4):
     adatbázis.close()
 
 print("************Üdvözlöm program szó és mondat megadó részlegén!************")
-    
+
+
 def adatka():
     global szó
     global mondat1
@@ -31,5 +34,56 @@ def adatka():
         adatokbe(szó,mondat1,mondat2,mondat3,mondat4)
         if szó!="":
             return adatka()
-adatka()
+
+def regisztráció():
+    email = input("Írja be az email címét: ")
+    jelszó = input("Adjon meg egy jelszót: ")
+    biztjelszó = input("Újra a jelszót: ")
+    
+    if biztjelszó == jelszó:
+        enc = biztjelszó.encode()
+        hash1 = hashlib.md5(enc).hexdigest()
+        
+        with open("beléptetőadatok.txt", "a") as f:
+            f.write(email + "|" + hash1 + "\n")
+        print("Sikeresen regisztrált!")
+    else:
+        print("A jelszó nem egyezik!\n")
+
+def bejelentkezés():
+    email = input("Írja be az email címét: ")
+    jelszó = input("Írja be a jelszavát: ")
+    auth = jelszó.encode()
+    auth_hash = hashlib.md5(auth).hexdigest()
+
+    with open("beléptetőadatok.txt", "r") as f:
+        adatok = f.readlines()
+
+    for sor in adatok:
+        tárolt_email, tárolt_jelszó = sor.strip().split("|")
+        if email == tárolt_email and auth_hash == tárolt_jelszó:
+            print("Sikeresen bejelentkezett!")
+            adatka()
+            break
+        else:
+            print("Sikertelen bejelentkezés!\n")
+
+while 1:
+    print("********** Login System **********")
+    print("1.Regisztráció")
+    print("2.Bejelentkezés")
+    print("3.Kilépés")
+    
+    választás = int(input("Válasszon a lehetőségek közül: "))
+    
+    if választás == 1:
+        regisztráció()
+    elif választás == 2:
+        bejelentkezés()
+    elif választás == 3:
+        break
+    else:
+        print("Rossz válasz!")
+
+
 
